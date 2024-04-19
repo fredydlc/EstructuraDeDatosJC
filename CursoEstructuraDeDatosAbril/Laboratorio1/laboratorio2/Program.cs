70665751bb84b536aa365842af4f87e3f65972a7
fredydlc <fredy.delacruz.27@unsch.edu.pe>
2024-04-19 12:01:18 -0500

    sumadenumeros

---
+++ CursoEstructuraDeDatosAbril/Laboratorio1/laboratorio2/Program.cs
@@ -0,0 +1,10 @@
+﻿//programa que sume los n primero numeros
+var n = 10;
+var suma = 0;
+
+for (var i = 0; i <= n; i++) { suma = suma + i; }
+
+Console.WriteLine("El resultado es: " + suma.ToString());
+
+
+
